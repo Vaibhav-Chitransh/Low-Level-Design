6c1a0795b68820db6ed7bf4e488e5cd30bca1486
Vaibhav Chitransh <vaibhavchitranshbdn@gmail.com>
2025-08-26 15:43:17 +0530

    singleton design pattern

---
+++ Singleton Design Pattern/ThreadSafeDoubleLockingSingleton.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<mutex>
+using namespace std;
+
+class Singleton {
+private:
+    static Singleton* instance;
+    static mutex mtx;
+
+    Singleton() {
+        cout << "Singleton Constructor called." << endl;
+    }
+
+public:
+    static Singleton* getInstance() {
+        if(instance == nullptr) {  // first check (no locking)
+            std::lock_guard<mutex> lock(mtx);  // lock only if needed
+            if(instance == nullptr) {  // second check (after acquiring lock)
+                instance = new Singleton();
+            }
+        }
+
+        return instance;
+    }
+};
+
+Singleton* Singleton::instance = nullptr;
+mutex Singleton::mtx;
+
+int main() {
+    Singleton* s1 = Singleton::getInstance();
+    Singleton* s2 = Singleton::getInstance();
+
+    cout << (s1 == s2) << endl;
+    
+    return 0;
+}
---
+++ Singleton Design Pattern/ThreadSafeLockingSingleton.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<mutex>
+using namespace std;
+
+class Singleton {
+private:
+    static Singleton* instance;
+    static mutex mtx;
+
+    Singleton() {
+        cout << "Singleton Constructor called." << endl;
+    }
+
+public:
+    static Singleton* getInstance() {
+        std::lock_guard<mutex> lock(mtx);
+        if(instance == nullptr) {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+};
+
+Singleton* Singleton::instance = nullptr;
+mutex Singleton::mtx;
+
+int main() {
+    Singleton* s1 = Singleton::getInstance();
+    Singleton* s2 = Singleton::getInstance();
+
+    cout << (s1 == s2) << endl;
+    
+    return 0;
+}
